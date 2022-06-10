cbada66576296fdfc111b0abda5617f9f9172f1e
ahmed <ahmedkhaled8901@gmail.com>
2022-06-10 23:38:22 +0200

    add home app to INSTALLED_APPS in mysite/setting.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.home.PollsConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
