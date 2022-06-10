dd83acca672cc7ce682ed460cf4589b5e023163a
ahmed <ahmedkhaled8901@gmail.com>
2022-06-11 01:42:25 +0200

    Implement myview function to create session for session assignment

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse
 from urllib3 import HTTPResponse
 
 # Create your views here.
-def myview(request):
-    return HttpResponse('hello from hello app')
+def myview(request) :
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits 
+    if num_visits > 4 : del(request.session['num_visits'])
+    resp = HttpResponse('view count='+str(num_visits))
+    resp.set_cookie('dj4e_cookie', '3ea0dca5', max_age=1000)
+    return resp
+
