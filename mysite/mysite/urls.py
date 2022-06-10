21f32f66a9c519fe83b4f72a6d70bae7bbb5af25
ahmed <ahmedkhaled8901@gmail.com>
2022-06-10 23:32:04 +0200

    add the root path(/) to mysite/urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     
