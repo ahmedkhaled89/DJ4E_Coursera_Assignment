d8f8d84c3d4c0a29e177f7225a508128fc9935d3
ahmed <ahmedkhaled8901@gmail.com>
2022-06-11 00:23:31 +0200

    add myview function to hoello/views.py

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from requests import request
+from django.http import HttpResponse
+from urllib3 import HTTPResponse
 
 # Create your views here.
+def myview(request):
+    return HttpResponse('hello from hello app')
