542256118792d7ccea7d3eb8af3196a656d54bab
ahmed <ahmedkhaled8901@gmail.com>
2022-06-24 20:30:01 +0200

    add add the accounts/ path

--- Course_3_Assignments/mysite/mysite/urls.py
+++ Course_3_Assignments/mysite/mysite/urls.py
@@ -3,10 +3,14 @@ from django.urls import include, path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
+
     path('', TemplateView.as_view(template_name='home/main.html')),
-   path('autos/', include('autos.urls')),
-   path('hello/', include('hello.urls')),
-   path('polls/', include('polls.urls')),
+    path('autos/', include('autos.urls')),
+    path('hello/', include('hello.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # Add
+
+
     
 ]
