0cf5acc6fdf2f837e906057439b34e8319febaf9
ahmed <ahmedkhaled8901@gmail.com>
2022-06-26 01:27:50 +0200

    add CatList View Class

--- Course_3_Assignments/mysite/cats/views.py
+++ Course_3_Assignments/mysite/cats/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Cat, Breed
 # Create your views here.
 
 def testview(req):
     return HttpResponse("hello from cats app")
+
+class CatList(LoginRequiredMixin, View):
+    def get(self, request):
+        cat_list = Cat.objects.all()
+        breed_count = Breed.objects.all().count()
+        cnx = { "cats": cat_list, "breed_count": breed_count}
+        return render(request, "cats/cat_list.html", cnx)
+
