1bd9a446b57c979093986319b8dacc6316dc725f
ahmed <ahmedkhaled8901@gmail.com>
2022-06-25 05:14:48 +0200

    add MainView to views.py

--- Course_3_Assignments/mysite/autos/views.py
+++ Course_3_Assignments/mysite/autos/views.py
@@ -1,6 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-#from . import models
-from .models import Auto, Make
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from autos.models import Auto, Make
 # Create your views here.
+
+class MainView(LoginRequiredMixin, View):
+    def get(self, request):
+        mc = Make.objects.all().count()
+        al = Auto.objects.all()
+
+        ctx = {'make_count': mc, 'auto_list': al}
+        return render(request, 'autos/auto_list.html', ctx)
+
 
