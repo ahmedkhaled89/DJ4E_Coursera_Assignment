0cf70f0f7a7dfd6b3805af6b42b0f59501ca9bab
ahmed <ahmedkhaled8901@gmail.com>
2022-06-26 06:36:12 +0200

    add CreateBreed View

--- Course_3_Assignments/mysite/cats/urls.py
+++ Course_3_Assignments/mysite/cats/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'cats'
 urlpatterns = [
-    path('', views.CatList.as_view(), name='allcats' )
+    path('', views.CatList.as_view(), name='all' ),
+    path('create', views.BreedCreate.as_view(), name="breed_create")
 ]
