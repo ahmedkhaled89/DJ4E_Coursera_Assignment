5dd5583b71046de8e1b30c2ee513309cd0cf4819
ahmed <ahmedkhaled8901@gmail.com>
2022-06-26 18:08:06 +0200

    Edit urls patterns

--- Course_3_Assignments/mysite/cats/urls.py
+++ Course_3_Assignments/mysite/cats/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = 'cats'
 urlpatterns = [
     path('', views.CatList.as_view(), name='all' ),
-    path('create', views.BreedCreate.as_view(), name="breed_create"),
-    path('breeds', views.BreedList.as_view(), name= 'breed_list'),
+    path('lookup/create', views.BreedCreate.as_view(), name="breed_create"),
+    path('lookup', views.BreedList.as_view(), name= 'breed_list'),
 ]
