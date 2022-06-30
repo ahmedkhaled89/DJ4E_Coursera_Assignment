bc80605c1e1f4f874492c114a4060fc56a4ac507
ahmed <ahmedkhaled8901@gmail.com>
2022-06-30 18:17:14 +0200

    Finishing Cat assingment

--- Course_3_Assignments/mysite/cats/models.py
+++ Course_3_Assignments/mysite/cats/models.py
@@ -9,7 +9,7 @@ class Breed(models.Model):
 
 class Cat(models.Model):
     nickname = models.CharField(max_length=200, validators = [MinLengthValidator(2,"Nickname must be greater than 1 character")])
-    wieght = models.PositiveIntegerField()
+    weight = models.PositiveIntegerField()
     foods = models.CharField(max_length=300)
     breed = models.ForeignKey('Breed', on_delete=models.CASCADE, null=False)
 
